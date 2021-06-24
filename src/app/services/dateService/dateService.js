a8da8e22fb233919543035d201326cb6ef53b210
Maxi <maxi.gatto96@gmail.com>
2021-06-24 20:55:06 -0300

    [FT] Agregado dateService

---
+++ src/app/services/dateService/dateService.js
@@ -0,0 +1,18 @@
+
+
+export const getCurrentMonthAndYear = () => {
+    const date = new Date();
+    const today = date.toISOString().slice(0,7);
+    return today;
+}
+
+export const getPreviousMonthAndYear = () => {
+    const date = new Date();
+    let month = date.getMonth();
+    const year = date.getFullYear();
+
+    if(month < 10)  month = "0" + month;
+    
+
+    return `${year}-${month}`
+}
