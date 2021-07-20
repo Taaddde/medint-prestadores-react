d821bd2d883919b26889d85b67e92b5a3261ebf1
Tadeo <taaddde@hotmail.com>
2021-07-19 23:40:15 -0300

    [FX] Return en map

--- src/app/services/dateService/dateService.js
+++ src/app/services/dateService/dateService.js
@@ -26,7 +26,7 @@ export const castDates = (sesiones) => {
 }
 
 export const castFechayHora = (datos) => {
-    datos.map(dato => dato.fechadecarga = dato.fechadecarga.replace(/^(\d{4})-(\d{2})-(\d{2})$/g,'$3/$2/$1') );
+    datos.map(dato => {return dato.fechadecarga = dato.fechadecarga.replace(/^(\d{4})-(\d{2})-(\d{2})$/g,'$3/$2/$1')} );
 }
 
 export const castDatesDocumentos = (documentos) => {
